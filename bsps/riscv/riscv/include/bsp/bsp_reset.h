bbdbf53bacfa0f0e681078d9730a1639c1d677bb
purviyeshi <purviystudy@gmail.com>
2024-08-25 12:31:49 +0530

    Port reset code from HSS to RTEMS

---
+++ bsps/riscv/riscv/include/bsp/bsp_reset.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 embedded brains GmbH & Co. KG
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/**
+ * \file MPFS HSS Embedded Software - Types
+ * \brief MPFS HSS Embedded Software - Types
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>     // ptrdiff_t
+
+/**
+ * \brief HSS Cores Enumeration
+ */
+enum HSSHartId {
+    HSS_HART_E51 = 0,
+    HSS_HART_U54_1,
+    HSS_HART_U54_2,
+    HSS_HART_U54_3,
+    HSS_HART_U54_4,
+
+    HSS_HART_NUM_PEERS,
+    HSS_HART_ALL = HSS_HART_NUM_PEERS
+};
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ bsps/riscv/riscv/start/bsp_reset.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2018 embedded brains GmbH & Co. KG
+ *
+ * Copyright (c) 2015 University of York.
+ * Hesham Almatary <hesham@alumni.york.ac.uk>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <bsp/bsp_reset.h>
+// #include <assert.h>
+#include <bsp/reset.h>
+#include <bsp/mss_sysreg1.h>
+// #include "/home/purva/quick-start/src/rtems/bsps/riscv/riscv/include/bsp/mss_sysreg.h"
+
+__attribute__((__noreturn__)) static void HSS_reboot_cold_all(void)
+{
+    // Trigger a full MSS reset by writing 0xDEAD to the MSS_RESET_CR register.
+    SYSREG->MSS_RESET_CR = 0xDEAD;
+
+    // Print the value of SYSREG to check if it's correctly initialized
+    printf("SYSREG pointer: %p\n", (void *)SYSREG);
+
+    // Infinite loop to ensure the function does not return after the reset.
+    while (1) { ; }
+}
+
+
+void HSS_reboot_cold(enum HSSHartId target)
+{
+    if (target == HSS_HART_ALL) {
+        HSS_reboot_cold_all();
+    } else {
+        printf("Problem in Soft Reboot");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
